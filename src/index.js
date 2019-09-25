d76170f26c8e4fcedddc6951d226465e39e4de6d
Alex Angelo <alexangelo87@gmail.com>
2019-09-24 23:44:49 -0300

    colors and axios

--- src/index.js
+++ src/index.js
@@ -3,8 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import green from '@material-ui/core/colors/green';
+const theme = createMuiTheme({
+    palette: {
+      primary: purple,
+      secondary: green,
+    },
+    status: {
+      danger: 'orange',
+    },
+  });
+ReactDOM.render(
+    <MuiThemeProvider theme= {theme}>
+        <App />
+    </MuiThemeProvider>,
+     document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
